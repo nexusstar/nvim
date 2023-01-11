7a6d28b5b7573524e82f6c9a4fa2f0ef7be9f1d2
Petar Nikov <petar.nikov@gmail.com>
2023-01-11 22:30:58 +0100

    chore: update telescope config, add recent files

--- init.lua
+++ init.lua
@@ -411,7 +411,13 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader><space>', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').buffers(require('telescope.themes').get_dropdown {
+    previewer = false
+  })
+end,
+  { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -425,6 +431,11 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>or', function()
+  require('telescope.builtin').oldfiles(require('telescope.themes').get_dropdown{
+    winblend = 10
+  })
+end, { desc = '[O]pen [R]ecent files' })
 
 -- File tree opened
 -- see ':help nvim-tree'
