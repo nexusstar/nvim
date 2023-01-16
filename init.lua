03292a30d7146ebffe76060b288271ce2e5250e5
Petar Nikov <petar.nikov@gmail.com>
2023-01-16 11:11:08 +0200

    Keymap for uppercase in insert mode

--- init.lua
+++ init.lua
@@ -256,6 +256,10 @@ vim.g.maplocalleader = ' '
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- set uppercase on insert mode after typing
+-- absolutly love this one
+vim.keymap.set('i', '<C-u>', '<Esc>gUiw`]a', { silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
